04e09a4a08a16811bcd6b81bb0b7359bdef89c3d
mguadagni <guadagnimichael@gmail.com>
2022-04-18 21:53:16 -0400

    Completed Error Handling class and GET and DELETE mappings

--- src/main/java/com/careerdevs/gorestsql/controllers/UserController.java
+++ src/main/java/com/careerdevs/gorestsql/controllers/UserController.java
@@ -2,14 +2,15 @@ package com.careerdevs.gorestsql.controllers;
 
 import com.careerdevs.gorestsql.models.User;
 import com.careerdevs.gorestsql.repos.UserRepository;
+import com.careerdevs.gorestsql.utils.ApiErrorHandling;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping ("/user")
@@ -23,18 +24,106 @@ public class UserController {
 
         try {
 
+            if(ApiErrorHandling.isStrNaN(id)) {
+
+                throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, id + " is not a valid ID");
+
+            }
+
             int uID = Integer.parseInt(id);
+
+            Optional<User> foundUser = userRepository.findById(uID);
+
+            if (foundUser.isEmpty()) {
+
+                throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, id + " is not a valid ID");
+
+            }
+
+            return new ResponseEntity<>(foundUser, HttpStatus.OK);
+
+        } catch (HttpClientErrorException e) {
+
+            return ApiErrorHandling.customApiError(e.getMessage(), e.getStatusCode());
+
+        } catch (Exception e){
+
+            return ApiErrorHandling.genericApiError(e);
+
+        }
+
+    }
+
+    @DeleteMapping ("/{id}")
+    public ResponseEntity<?> deleteUserById (@PathVariable ("id") String id) {
+
+        try {
+
+            if(ApiErrorHandling.isStrNaN(id)) {
+
+                throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, id + " is not a valid ID");
+
+            }
+
+            int uID = Integer.parseInt(id);
+
+            Optional<User> foundUser = userRepository.findById(uID);
+
+            if (foundUser.isEmpty()) {
+
+                throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, id + " is not a valid ID");
+
+            }
+
+            userRepository.deleteById(uID);
+
+            return new ResponseEntity<>(foundUser, HttpStatus.OK);
+
+        } catch (HttpClientErrorException e) {
+
+            return ApiErrorHandling.customApiError(e.getMessage(), e.getStatusCode());
+
+        } catch (Exception e){
+
+            return ApiErrorHandling.genericApiError(e);
+
+        }
+
+    }
+
+    @PostMapping("/upload/{id}")
+    public ResponseEntity<?> uploadUserById (
+            @PathVariable ("id") String userId,
+            RestTemplate restTemplate
+    ) {
+
+        try {
+
+            if(ApiErrorHandling.isStrNaN(userId)) {
+
+                throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, userId + " is not a valid ID");
+
+            }
+
+            int uID = Integer.parseInt(userId);
 
             String url = "https://gorest.co.in/public/v2/users/" + uID;
 
             User foundUser = restTemplate.getForObject(url, User.class);
 
-            System.out.println(foundUser);
+            if (foundUser == null) {
 
-            assert foundUser != null;
-            userRepository.save(foundUser);
+                throw new HttpClientErrorException(HttpStatus.NOT_FOUND, "User data was null");
 
-            return new ResponseEntity<>("Temp", HttpStatus.OK);
+            }
+
+            User savedUser = userRepository.save(foundUser);
+
+            return new ResponseEntity<>(savedUser, HttpStatus.OK);
+
+        } catch (HttpClientErrorException e) {
+
+            return ApiErrorHandling.customApiError(e.getMessage(), e.getStatusCode());
 
         } catch (NumberFormatException e) {
 
@@ -42,10 +131,7 @@ public class UserController {
 
         } catch (Exception e){
 
-            System.out.println(e.getMessage());
-            System.out.println(e.getClass());
-
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+            return ApiErrorHandling.genericApiError(e);
 
         }
 
@@ -61,10 +147,7 @@ public class UserController {
 
         } catch (Exception e) {
 
-            System.out.println(e.getMessage());
-            System.out.println(e.getClass());
-
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+            return ApiErrorHandling.genericApiError(e);
 
         }
 
--- src/main/java/com/careerdevs/gorestsql/utils/ApiErrorHandling.java
+++ src/main/java/com/careerdevs/gorestsql/utils/ApiErrorHandling.java
@@ -1,7 +1,45 @@
 package com.careerdevs.gorestsql.utils;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 public class ApiErrorHandling {
 
+    public static ResponseEntity<?> genericApiError (Exception e) {
 
+        System.out.println(e.getMessage());
+        System.out.println(e.getClass());
+
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+
+    }
+
+    public static ResponseEntity<?> customApiError (String message, HttpStatus status) {
+
+        return new ResponseEntity<>(message, status);
+
+    }
+
+    public static boolean isStrNaN (String strNum) {
+
+        if (strNum == null) {
+
+            return true;
+
+        }
+
+        try {
+
+            Integer.parseInt(strNum);
+
+        } catch (NumberFormatException e) {
+
+            return true;
+
+        }
+
+        return false;
+
+    }
 
 }
