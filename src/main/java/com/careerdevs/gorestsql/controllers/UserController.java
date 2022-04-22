9525f787448b96975197a39cb2da0e72cb769656
mguadagni <guadagnimichael@gmail.com>
2022-04-22 12:33:47 -0400

    Finished User Resource

--- src/main/java/com/careerdevs/gorestsql/controllers/UserController.java
+++ src/main/java/com/careerdevs/gorestsql/controllers/UserController.java
@@ -4,12 +4,17 @@ import com.careerdevs.gorestsql.models.User;
 import com.careerdevs.gorestsql.repos.UserRepository;
 import com.careerdevs.gorestsql.utils.ApiErrorHandling;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.Optional;
 
 @RestController
@@ -130,6 +135,105 @@ public class UserController {
             return new ResponseEntity<>("ID must be a number", HttpStatus.NOT_FOUND);
 
         } catch (Exception e){
+
+            return ApiErrorHandling.genericApiError(e);
+
+        }
+
+    }
+
+    @PostMapping ("/")
+    public ResponseEntity<?> createNewUser (@RequestBody User newUser) {
+
+        try{
+
+            User savedUser = userRepository.save(newUser);
+
+            return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+
+        } catch(HttpClientErrorException e) {
+
+            return ApiErrorHandling.customApiError(e.getMessage(), e.getStatusCode());
+
+        } catch (Exception e) {
+
+            return ApiErrorHandling.genericApiError(e);
+
+        }
+
+    }
+
+    @PutMapping ("/")
+    public ResponseEntity<?> updateUser (@RequestBody User updateUser) {
+
+        try{
+
+            User savedUser = userRepository.save(updateUser);
+
+            return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+
+        } catch(HttpClientErrorException e) {
+
+            return ApiErrorHandling.customApiError(e.getMessage(), e.getStatusCode());
+
+        } catch (Exception e) {
+
+            return ApiErrorHandling.genericApiError(e);
+
+        }
+
+    }
+
+    @PostMapping ("/uploadall")
+    public ResponseEntity<?> uploadAll (
+            RestTemplate restTemplate
+    ){
+
+        try {
+
+            String url = "http://gorest.co.in/public/v2/users";
+
+            ResponseEntity<User[]> response = restTemplate.getForEntity(url, User[].class);
+
+            User[] firstPageUsers = response.getBody();
+
+            if (firstPageUsers == null) {
+
+                throw new HttpClientErrorException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to get first page of users");
+
+            }
+
+            ArrayList<User> allUsers = new ArrayList<>(Arrays.asList(firstPageUsers));
+
+            HttpHeaders responseHeaders = response.getHeaders();
+
+            String totalPages = Objects.requireNonNull(responseHeaders.get("X-Pagination-Pages")).get(0);
+            int totalPgNum = Integer.parseInt(totalPages);
+
+            for (int i = 2; i <= totalPgNum; i++) {
+
+                String pageUrl = url + "?page=" + i;
+                User[] pageUsers = restTemplate.getForObject(pageUrl, User[].class);
+
+                if (pageUsers == null) {
+
+                    throw new HttpClientErrorException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to get page " + i + " of users");
+
+                }
+
+                allUsers.addAll(Arrays.asList(firstPageUsers));
+
+            }
+
+            userRepository.saveAll(allUsers);
+
+            return new ResponseEntity<>("Users Created: " + allUsers.size(), HttpStatus.OK);
+
+        } catch(HttpClientErrorException e) {
+
+            return ApiErrorHandling.customApiError(e.getMessage(), e.getStatusCode());
+
+        } catch (Exception e) {
 
             return ApiErrorHandling.genericApiError(e);
 
